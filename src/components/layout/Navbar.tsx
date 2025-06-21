13791a8a51249d904354cbe67051c3b3bafe8599
yogui-dev <jorge.m.arias.p@gmail.com>
2025-06-20 23:07:09 -0400

    refactor: Añade estilo para el enlace activo en la navegación

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,29 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 mb-4">
       <div className="container mx-auto flex justify-between">
-        <Link to="/" className="text-white font-bold">Mi App</Link>
+        <NavLink to="/" className="text-white font-bold">
+          Mi App
+        </NavLink>
         <div>
-          <Link to="/" className="text-gray-300 hover:text-white mr-4">Inicio</Link>
-          <Link to="/about" className="text-gray-300 hover:text-white">Acerca de</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `mr-4 ${isActive ? 'text-yellow-400 font-bold' : 'text-gray-300 hover:text-white'}`
+            }
+          >
+            Inicio
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              `${isActive ? 'text-yellow-400 font-bold' : 'text-gray-300 hover:text-white'}`
+            }
+          >
+            Acerca de
+          </NavLink>
         </div>
       </div>
     </nav>
